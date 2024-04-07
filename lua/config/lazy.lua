fe081f8d6e5847bb64d81699d444065c788fc38a
leninner <mazabandalenin180@gmail.com>
2024-04-07 12:21:52 -0500

    Update color scheme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,12 +11,14 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
-      colorscheme = "solarized-osaka",
+      colorscheme = "tokyonight",
     } },
     -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,5 @@
 return {
-  "craftzdog/solarized-osaka.nvim",
+  "folke/tokyonight.nvim",
   lazy = true,
   priority = 1000,
   opts = function()

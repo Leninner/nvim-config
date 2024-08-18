a3a9da294520745e95e19a73d9406b94a6b5a807
leninner <mazabandalenin180@gmail.com>
2024-08-17 21:51:05 -0500

    feat: update search options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,8 @@
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+-- Search and replace
+vim.opt.ignorecase = true -- search case insensitive
+vim.opt.smartcase = true -- search matters if capital letter is used
+vim.opt.inccommand = "split" -- for incsearch while sub
--- lua/leninner/discipline.lua
+++ lua/leninner/discipline.lua
@@ -5,7 +5,7 @@ function M.cowboy()
   local id
   local ok = true
 
-  for _, key in ipairs({ "h", "j", "k", "l", "+", "-" }) do
+  for _, key in ipairs({ "h", "j", "k", "l", "+", "-", "w", "b" }) do
     local count = 0
     local timer = assert(vim.loop.new_timer())
     local map = key

4becd9db3e5f954e6028b0ad1696ca80cf85e0f5
leninner <mazabandalenin180@gmail.com>
2024-09-28 08:02:48 -0500

    chore: update coloscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,19 +1,36 @@
 return {
+  -- Select a colorscheme by setting colorscheme = "name"
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  -- onedarkpro
   {
     "olimorris/onedarkpro.nvim",
     priority = 1000,
     name = "onedark_vivid",
   },
+  -- tokyonight
   {
     "folke/tokyonight.nvim",
     lazy = true,
     name = "tokyonight",
     priority = 999,
-  },
-  {
-    "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark_vivid",
+      style = "storm",
+      transparent = true,
+    },
+  },
+  -- catppuccino
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "macchiato",
+      transparent_background = true,
     },
   },
 }

41b0299955673a1cf40b89b0d307fbde6bd43938
leninner <mazabandalenin180@gmail.com>
2024-11-25 10:13:06 -0500

    chore: add harpoon shortcuts

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,12 +3,44 @@
 -- Add any additional keymaps here
 
 local oil = require("oil")
+local harpoon = require("harpoon")
+
+harpoon:setup()
 local discipline = require("leninner.discipline")
 discipline.cowboy()
 oil.setup()
 
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
+
+-- Harpoon
+keymap.set("n", "<leader>a", function()
+  harpoon:list():add()
+end)
+keymap.set("n", "<C-e>", function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
+
+keymap.set("n", "<C-h>", function()
+  harpoon:list():select(1)
+end)
+keymap.set("n", "<C-t>", function()
+  harpoon:list():select(2)
+end)
+keymap.set("n", "<C-n>", function()
+  harpoon:list():select(3)
+end)
+keymap.set("n", "<C-s>", function()
+  harpoon:list():select(4)
+end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+keymap.set("n", "<C-S-P>", function()
+  harpoon:list():prev()
+end)
+keymap.set("n", "<C-S-N>", function()
+  harpoon:list():next()
+end)
 
 -- Select all
 keymap.set("n", "<C-a>", "gg<S-v>G")
